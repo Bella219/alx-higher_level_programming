c0f5c91d22792891bf065349fcad3c83572cd2eb
Bella219 <cho219bella@yahoo.com>
2022-12-01 13:23:08 -0800

    Program that prints the number of and the list of its arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import sys
 
     total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
+    for index in range(len(sys.argv) - 1):
+        total += int(sys.argv[index + 1])
     print("{}".format(total))
